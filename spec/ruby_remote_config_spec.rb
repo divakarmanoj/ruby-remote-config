39aff6750f970b82016aeed89031489ca64bc33a
manoj <manoj.prithvee@gmail.com>
2023-08-01 15:57:17 -0500

    Added mote tests

--- spec/ruby_remote_config_spec.rb
+++ spec/ruby_remote_config_spec.rb
@@ -10,5 +10,14 @@ describe RubyRemoteConfig do
       expect(client.repository.get_data("test")).to eq("test")
       client.stop
     end
+    it "Change the data in the file and check if the data is updated" do
+      client = RubyRemoteConfig::Client.new(
+        repository: Source::FileRepository.new(name: "test", path: "spec/test.yaml"),
+        refresh_interval: 1)
+      expect(client.repository.get_data("test")).to eq("test")
+      File.open("spec/test.yaml", "w") { |file| file.write("test: test1") }
+      expect(client.repository.get_data("test")).to eq("test1")
+      client.stop
+    end
   end
 end
